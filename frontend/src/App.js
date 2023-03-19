19c22adfa2b92a58f32145ac1e7c072227d98632
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-03-19 15:14:02 +0530

    Setup Router React Dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
   );
 }
 
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h1>This is home page</h1>
+  )
+}
+
+export default Home
