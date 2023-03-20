da1c433897211f419fdbfab625304daca3be8077
IT21183768 <it21183768@my.sliit.lk>
2023-03-20 14:20:46 +0530

    Add Checkout Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
+=======
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Showroom from "./pages/Showroom/Showroom";
+import Checkout from "./pages/Checkout/Checkout";
+>>>>>>> Stashed changes
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,6 +27,14 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/showroom" element={<Showroom/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+      </Routes>
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ frontend/src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
+import './checkout.css'
+
+const Checkout = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Checkout
