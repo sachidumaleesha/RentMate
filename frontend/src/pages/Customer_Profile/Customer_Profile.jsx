10c1152e462e71c94316551687837fa7a8bc27bb
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-03-20 14:54:36 +0530

    Link Files
    Add Route Path to App.js
    Link CSS file to Property.jsx

--- frontend/src/pages/Customer_Profile/Customer_Profile.jsx
+++ frontend/src/pages/Customer_Profile/Customer_Profile.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
 
-import React from 'react'
-
 const Customer_Profile = () => {
   return (
 
--- frontend/src/pages/Property/Property.jsx
+++ frontend/src/pages/Property/Property.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
+import './property.css'
 
 const Property = () => {
   return (
     <div>
-        
+        <Navbar/>
+        <h2>Content Goes Here</h2>
+        <Footer/>
     </div>
   )
 }
