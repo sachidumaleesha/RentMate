f882fa002e019c15bcf63c4c9ff663da53bec94a
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-03-20 15:02:18 +0530

    Set the Structure

--- frontend/src/pages/Property/Property.jsx
+++ frontend/src/pages/Property/Property.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Property = () => {
   return (
     <div>
+<<<<<<< Updated upstream
         
+=======
+        <Navbar/>
+        <h2>Content Goes Here</h2>
+        {/* Header */}
+        {/* Body Content */}
+        {/* End part */}
+        <Footer/>
+>>>>>>> Stashed changes
     </div>
   )
 }
