5ebfd79c1076fb8580031951bbc1be6899ed7465
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-03-21 12:25:52 +0530

    Update the footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './footer.css'
+import Visa from './images/visa.png'
+import Mastercard from './images/mastercard.png'
+import Discover from './images/discover.png'
+import AmericanExpress from './images/american-express.png'
 
 const Footer = () => {
   return (
@@ -11,22 +15,25 @@ const Footer = () => {
             <li><a href="#">Our Story</a></li>
             <li><a href="#">Our Team</a></li>
             <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Feedback</a></li>
           </ul>
         </div>
         <div className="footer-column">
           <h3>Services</h3>
           <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">SEO</a></li>
-            <li><a href="#">Social Media</a></li>
+            <li><a href="#">Propeties</a></li>
+            <li><a href="#">Transport</a></li>
+            <li><a href="#">Lawyers</a></li>
+            <li><a href="#">Furniture Showrooms</a></li>
           </ul>
         </div>
         <div className="footer-column">
           <h3>Resources</h3>
           <ul>
             <li><a href="#">Blog</a></li>
-            <li><a href="#">Ebooks</a></li>
-            <li><a href="#">FAQs</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Guidlines</a></li>
+            <li><a href="#">Support Center</a></li>
           </ul>
         </div>
         <div className="footer-column">
@@ -39,12 +46,13 @@ const Footer = () => {
       </div>
       <div className="footer-bottom">
         <div className="payment-methods">
-          <img src="https://www.example.com/images/payment-methods.png" alt="Accepted payment methods" />
-        </div>
-        <div className="social-buttons">
-          <a href="#"><i className="fab fa-facebook"></i></a>
-          <a href="#"><i className="fab fa-twitter"></i></a>
-          <a href="#"><i className="fab fa-instagram"></i></a>
+          <div className="social-buttons">
+            <a href="#"><i className="pi pi-facebook"></i></a>
+            <a href="#"><i className="pi pi-instagram"></i></a>
+            <a href="#"><i className="pi pi-send"></i></a>
+            <a href="#"><i className="pi pi-reddit"></i></a>
+            <a href="#"><i className="pi pi-twitter"></i></a>
+          </div>
         </div>
       </div>
     </footer>
