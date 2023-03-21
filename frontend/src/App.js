f1a75c9b9c43b0cbe28dbe26e32bebfb57b332e6
IT21186424 <99867312+IT21186424@users.noreply.github.com>
2023-03-21 08:29:21 +0530

    Add Blog Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
+=======
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Showroom from "./pages/Showroom/Showroom";
+import Checkout from "./pages/Checkout/Checkout";
+import Customer_Profile from "./pages/Customer_Profile/Customer_Profile";
+import Property from "./pages/Property/Property";
+import Add_blog from "./pages/Add_blog/Add_blog";
+>>>>>>> Stashed changes
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,6 +30,17 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/showroom" element={<Showroom/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/customer" element={<Customer_Profile/>} />
+        <Route path="/property" element={<Property/>} />
+        <Route path="/Add_blog" element={<Add_blog/>} />
+      </Routes>
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ frontend/src/pages/Add_blog/Add_blog.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './add_blog.css'
+import Navbar from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
+
+
+const Add_blog = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Add_blog
