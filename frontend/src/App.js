10ac742f2eed3bec8b7503cc1ae36027ffac9c7d
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-03-21 12:08:52 +0530

    Add path to navigate landlord dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Showroom from "./pages/Showroom/Showroom";
+import Checkout from "./pages/Checkout/Checkout";
+import Customer_Profile from "./pages/Customer_Profile/Customer_Profile";
+import Property from "./pages/Property/Property";
+import Dashboard from "./pages/Landlord_Dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/showroom" element={<Showroom/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/customer" element={<Customer_Profile/>} />
+        <Route path="/property" element={<Property/>} />
+
+        {/* Route Path for Landlord */}
+        <Route path="/landlord">
+          <Route path="dashboard" element={<Dashboard/>}></Route>
+        </Route>
+        {/* End of the route Path for Landlord */}
+      </Routes>
     </div>
   );
 }
